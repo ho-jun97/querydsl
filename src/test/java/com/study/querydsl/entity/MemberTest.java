fe6f2655c839a30ed4f87aa13fb1077d83795c13
ldr <hojun9707@naver.com>
2023-03-13 15:38:13 +0900

    test: Querydsl vs JPQL

---
+++ src/test/java/com/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,54 @@
+package com.study.querydsl.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional // test 가 끝나고 roll back 을 한다.
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void testEntity(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        // 초기화
+        em.flush(); // 영속성 컨턱스트에 있는 오브젝트들을 실제 쿼리를 만들어서 db에 날린다.
+        em.clear(); // 영속성 컨텍스트를 완전히 초기화해서 캐시가 날라감
+
+        List<Member> members = em.createQuery("select m  from Member m", Member.class)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.team" + member.getTeam());
+        }
+
+    }
+}
