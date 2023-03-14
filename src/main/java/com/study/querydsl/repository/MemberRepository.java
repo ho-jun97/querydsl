856341bb23d556d3b23e3beae78a90f8a5a44ed8
ldr <hojun9707@naver.com>
2023-03-14 14:41:44 +0900

    feat: Custom repository

---
+++ src/main/java/com/study/querydsl/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.study.querydsl.repository;
+
+import com.study.querydsl.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+
+    // select m from member m where m.username = ?
+    List<Member> findByUsername(String username);
+}
